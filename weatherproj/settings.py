36baf80ccbe5be17b510989e3d28b86d11ce3412
fznrsd@DESKTOP-DRD6N6C <fznrsd@DESKTOP-DRD6N6C.(MINGW64)git config --global user.name Faizan>
2021-10-26 13:15:01 +0530

    dd

--- weatherproj/settings.py
+++ weatherproj/settings.py
@@ -76,13 +76,14 @@ WSGI_APPLICATION = 'weatherproj.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+import dj_database_url
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-import dj_database_url
+
 
 
 
